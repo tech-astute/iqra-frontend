0fdfe2a6892f370dd3691c46cc7ffb1c5ad1dbae
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-12-17 00:59:58 +0530

    url dev

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/master',
+    // baseURL: 'http://localhost:5000/api/master',
+    baseURL: 'https://iqra-4t1k.onrender.com/api/master'
 });
 
 export const addArticle = (articleInfo) => api.post(`/add-articles`, articleInfo);
